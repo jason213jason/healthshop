4b6145af2c0b5fcf5a607256f4d1f6563a929bff
18320444515 <32980506+18320444515@users.noreply.github.com>
2018-12-03 22:41:18 +0800

    Revert "Revert "ZTY | 22:39""
    This reverts commit c998cc6f4df8cd53da840654f405937006021100.

--- js/search.js
+++ js/search.js
@@ -1,7 +1,3 @@
 ﻿$(function (){
-    $('.search-bar a').on('click',function(){
-        /*跳转去搜索列表 带上key*/
-        var key =$.trim($('input').val());
-        location.href = 'searchlist.html?key='+key;
-    });
+   
 });
