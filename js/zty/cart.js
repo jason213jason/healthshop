dd06ddc04ede3af61eb374de45f02e7f90239efa
18320444515 <32980506+18320444515@users.noreply.github.com>
2018-12-04 14:04:36 +0800

    ZTY | 14:04
    发布后会有bug，总是登录失效

--- js/zty/cart.js
+++ js/zty/cart.js
@@ -40,4 +40,50 @@ $(document).ready(function(){
             console.log(e);
         }
     });
+
+    $('#deal').click(function(){
+        var addressId = $('input[name="address_choose"]:checked').attr('value');
+        console.log(addressId);
+
+        $('input[class="son_check"]:checked').each(function(){
+            console.log($(this).attr('orderId'));
+            var orderId = $(this).attr('orderId');
+            var index = $(this).attr('index');
+            var test = {
+                    userId: userId,
+                    id: orderId,
+                    paid: true,
+                    buy: orders.list[index].buy,
+                    addressId: addressId
+                }
+                console.log(test);
+            $.ajax({
+                url:'health/orders/update',
+                type:'POST',
+                dataType:'json',
+                data:{
+                    userId: userId,
+                    id: orderId,
+                    paid: 1,
+                    buy: orders.list[index].buy,
+                    addressId: addressId
+                },
+                headers: {
+                    //Bearer是我的项目需要的,你可以按你的需求规范格式
+                    'token': cookie.get("token"),
+                },
+                success:function(result){
+                    console.log(result);
+                    
+                },
+                error:function(e){
+                    console.log(e);
+                }
+            });
+
+        });
+        alert("付款完成，等待发货");
+        location.href = 'myorder.html';
+        
+    });
 })
